9610fddc838fb1b97f8771ff819f85b13818425b
Chap Ambrose <chapambrose@gmail.com>
2021-04-20 16:48:59 -0500

    bump

--- main.go
+++ main.go
@@ -29,6 +29,24 @@ func main() {
 		}
 		rdb := redis.NewClient(opts)
 		rdb.Set(ctx, "First value", "value_1", 0)
+		
+		val1, err := rdb.Get(ctx, "value_1").Result()
+		if err == redis.Nil {
+			log.Println("value_1 does not exist")
+		} else if err != nil {
+			panic(err)
+		} else {
+			log.Println("value_1", val2)
+		}
+
 		rdb.Set(ctx, "Second value", "value_2", 0)
+		val2, err := rdb.Get(ctx, "value_2").Result()
+		if err == redis.Nil {
+			fmt.Println("value_2 does not exist")
+		} else if err != nil {
+			panic(err)
+		} else {
+			fmt.Println("value_2", val2)
+		}
 	}
 }
